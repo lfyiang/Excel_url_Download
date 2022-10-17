2f127e53d429009d159632937b96a247fd435234
Crike <lfyiang@gmail.com>
2022-10-17 08:45:28 +0800

    Update excel_url_download.py

--- excel_url_download.py
+++ excel_url_download.py
@@ -1,7 +1,7 @@
 import xlrd
 import requests
 
-read_path = "待下载表格.xls"
+read_path = "待下载表格.xls" #将待下载的表格文件放在与本文件同目录
 bk = xlrd.open_workbook(read_path)
 shxrange = range(bk.nsheets)
 sh = bk.sheet_by_index(0)
@@ -10,10 +10,10 @@ nrows = sh.nrows
 print("共有 "+str(nrows)+" 组数据；")
 for i in range(nrows):
     print("正在下载第"+str(i+1)+"组图片")
-    url1 = sh.cell_value(i, 6)  # 依次读取每行第5列的数据，也就是 URL
-    url2 = sh.cell_value(i, 7)  # 依次读取每行第5列的数据，也就是 URL
-    name1 = sh.cell_value(i, 2) + '-图片名称1'  # 读取图片名称
-    name2 = sh.cell_value(i, 2) + '-图片名称2'  # 读取图片名称
+    url1 = sh.cell_value(i, 6)  # 依次读取每行第5列的数据，也就是 URL所在的行
+    url2 = sh.cell_value(i, 7)  # 依次读取每行第6列的数据，也就是 URL所在的行
+    name1 = sh.cell_value(i, 2) + '-图片名称1'  # 读取图片名称标识字段，比方说姓名列
+    name2 = sh.cell_value(i, 2) + '-图片名称2'  # 读取图片名称标识字段，比方说姓名列
     f1 = requests.get(url1)  # 下载图片
     f2 = requests.get(url2)  # 下载图片
     pic_name1 = "./Down_Pic/" + name1 + "." + "jpg"  # 构造完整文件路径+名称
